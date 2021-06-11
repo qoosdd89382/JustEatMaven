29057fbd94d8380e5b7721a47dfcb97157a38397
Cherry <qoosdd89382@gmail.com>
2021-06-11 20:37:11 +0800

    Hibernate組態設定載入(建議DAO先不要轉Hibernate，之後轉Spring，但已部署好Hibernate，可先練習)

---
+++ src/main/java/com/hibernate/util/HibernateUtil.java
@@ -0,0 +1,30 @@
+package com.hibernate.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtil {
+	private static SessionFactory sessionFactory = buildSessionFactory();
+	private static SessionFactory buildSessionFactory() {
+		try {
+			final StandardServiceRegistry serviceRegistry = 
+					new StandardServiceRegistryBuilder().configure().build();
+			return new MetadataSources(
+					serviceRegistry).buildMetadata().buildSessionFactory();
+		} catch (Throwable e) {
+			e.printStackTrace();
+			throw new ExceptionInInitializerError(e);
+		}
+	}
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public static void closeSessionFactory() {
+		if(sessionFactory!=null) {
+			sessionFactory.close();
+		}
+	}
+}
